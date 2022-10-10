fa82138097632ac1b4ceea8dda369f8b3d4060e9
Xavier Ladores <xavierladores@Xaviers-MacBook-Pro.local>
2022-10-10 21:59:49 +1300

    Initializing project

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Quote Displayer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </div>
+  );
+}

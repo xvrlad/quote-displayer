d9501696664cc8d348fde2d2b285417bdc6d3f8c
Xavier Ladores <xavierladores@Xaviers-MacBook-Pro.local>
2022-10-12 11:32:23 +1300

    test pr rule

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Quote Displayer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-
+      test
       <Card />
     </div>
   );

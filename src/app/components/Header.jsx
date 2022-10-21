4a7c0a5dbbacea715d37bcce6a35f702a7373e16
xvrlad <69370722+xvrlad@users.noreply.github.com>
2022-10-22 02:26:15 +1300

    Added custom color palette

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className="flex p-8 justify-center bg-light-green">Header</div>;
+};
+
+export default Header;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import Card from '../app/components/Card';
+import AuthorCard from '../app/components/AuthorCard';
+import Header from '../app/components/Header';
 
 export default function Home() {
   return (
@@ -10,7 +12,9 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
+      <Header />
       <Card />
+      <AuthorCard />
     </div>
   );
 }
